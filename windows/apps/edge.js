78785217049a265b72a6ae4d48953a085a30fb78
陈思全 <98014938+siquan001@users.noreply.github.com>
2022-10-12 12:27:46 +0800

    Add files via upload

--- windows/apps/edge.js
+++ windows/apps/edge.js
@@ -4,3 +4,43 @@ function r(){
     document.body.style.height=window.innerHeight+'px';
 }
 r();
+var f=document.getElementById('frame');
+var h=['https://bing.com/'];
+document.querySelector('.top input').onkeydown=function(e){
+    if(e.keyCode==13){
+        e.preventDefault();
+        var a=this.value.trim();
+        if(!a) return;
+        if(a.indexOf('.')>0){
+            if(a.indexOf('://')==-1){
+                a='http://'+a;
+            }  
+        }else{
+            a='https://cn.bing.com/search?q='+a;
+        }
+        f.src=a;
+        h.push(a);
+    }
+}
+document.querySelectorAll('.links a').forEach(function(element) {
+    element.onclick=function(){
+        var l=this.getAttribute('data-href');
+        f.src=l;
+        h.push(l);
+    }
+});
+document.querySelector('.back').onclick=function(){
+    if(h.length==1) return;
+    f.src=h[h.length-2];
+    h.pop();
+}
+document.querySelector('.reload').onclick=function(){
+    f.src='about:blank';
+    f.onload=function(){
+        f.src=h[h.length-1];
+    }
+}
+document.querySelector('.home').onclick=function(){
+    f.src='https://cn.bing.com/';
+    h=['https://cn.bing.com/'];
+}
